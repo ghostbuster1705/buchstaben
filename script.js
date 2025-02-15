1509dc26ec594405ef13811e96507a705fb491d1
ghostbuster1705 <alexander17058917@gmail.com>
2025-02-15 19:38:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "PRO",
     "creativity",
     "life",
+    "Webniko",
   ];
 
   const text = new SplitType(".sticky p", { types: "words" });
